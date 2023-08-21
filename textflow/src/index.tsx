7ff07d6b7b374d34e80660b4caca4ed70bbec2b7
Keanu Clark <da1andonlyone13@gmail.com>
2023-08-21 19:05:23 -0400

    react app testing folder restructuring

--- textflow/src/index.tsx
+++ textflow/src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Amplify, Storage } from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
