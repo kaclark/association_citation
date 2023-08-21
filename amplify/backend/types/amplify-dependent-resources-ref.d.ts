01adde3ab1456799f6312641e0a2fa40a53b7203
Keanu Clark <da1andonlyone13@gmail.com>
2023-08-21 13:33:05 -0400

    amplify integration testing

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "textflow": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3textflowstorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
