371f631e611987b8cb7e6d14bf72861aa54d32bc
Keanu Clark <da1andonlyone13@gmail.com>
2023-08-22 20:17:05 -0400

    api loads; data can be rendered; need to pre-load data before screen render

--- textflow/src/App.tsx
+++ textflow/src/App.tsx
@@ -1,33 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import token from "./token.json";
 import { TodoistApi } from '@doist/todoist-api-typescript'
 
-function parse_todo(comments) {
-	let comm_arr:string[] = new Array(comments.length);
-	comments.forEach(x => { comm_arr.push(x.content); });
-	return comm_arr;
-}
-
-function get_comments(){
-    const api = new TodoistApi(token.token)
-    let comment_set = api.getComments({ taskId: "7139027260"})
-	    .then((comments) => parse_todo(comments))
-	    .catch((error) => console.log(error))
-    console.log(comment_set)
-}
-
-get_comments()
+var comm_arr:string[] = [];
+//const [mcomm, setmcomm] = useState<Array<string>>([]);
+const api = new TodoistApi(token.token)
+api.getComments({ taskId: "7139027260"})
+	.then((comments) => {
+		//comments.forEach(x => { setmcomm(mcomm => [...mcomm, x.content]); });
+		comments.forEach(x => { comm_arr.push(x.content);})
+	})
+	.catch((error) => console.log(error))
+console.log(comm_arr)
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-	Coming Soon!
-        </p>
+	<ul>
+          {comm_arr.map(item => <li key={item}>{item}</li>)}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
