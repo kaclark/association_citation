66fcf56ad199631579dc74678df86eaf5f313aab
Keanu Clark <da1andonlyone13@gmail.com>
2023-09-21 23:30:17 -0400

    cleaned up script

--- curl_todoist_testing.sh
+++ curl_todoist_testing.sh
@@ -4,11 +4,9 @@
 #input Bearer token from json(protected with .gitignore)
 TOKEN=$(jq -r .token token.json)
 
-#jq -r '.[] | "\(.content)"'
 pull_reading_journal () {
-    COMMENTS=$(curl "https://api.todoist.com/rest/v2/comments?task_id=7250931521" -H "Authorization: Bearer $TOKEN" | jq -r '.[] | "\(.content)@@@"')
-    #for some reason the @@@ gets replaced but the newline character does not seem to register
-    echo -e $(echo -e $COMMENTS | tr "@@@" "\n")
+#issue: newline not rendering!!
+    echo -e $(curl "https://api.todoist.com/rest/v2/comments?task_id=7250931521" -H "Authorization: Bearer $TOKEN" | jq -r '.[] | "\(.content)\n"')
 
 }
 
