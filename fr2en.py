bfe617f546106277355c78d2c5bc39582d1bb4b9
Keanu Clark <da1andonlyone13@gmail.com>
2023-11-23 10:42:01 -0500

    added script that translates french to english and tracks history in a csv

---
+++ fr2en.py
@@ -0,0 +1,14 @@
+#usage: python fr2en.py "mot1,mot2,..."
+from PyMultiDictionary import MultiDictionary
+import sys
+
+input_words = sys.argv[1].split(",")
+dictionary=MultiDictionary()
+dictionary.set_words_lang('fr') 
+
+for iword in input_words:
+    trans = dictionary.translate('fr', iword)
+    trans_map = {x[0]:x[1] for x in trans}
+    print(iword, trans_map["en"])
+    with open("fr2en.csv", "a") as track_trans:
+        track_trans.write(iword + "," + trans_map["en"] + "\n")
