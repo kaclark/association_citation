341c0b5b01ad47b427fd796aaf11cbbadb3c975d
Keanu Clark <da1andonlyone13@gmail.com>
2023-11-28 22:04:17 -0500

    removed javascript related elements

--- textflow/todoist.ts
+++
@@ -1,9 +0,0 @@
-import { TodoistApi } from '@doist/todoist-api-typescript'
-import * as fs from 'fs';
-const rawToken = fs.readFileSync('token.api', 'utf-8');
-const token = rawToken.trim()
-const api = new TodoistApi(token)
-
-api.getTasks()
-    .then((tasks) => console.log(tasks))
-    .catch((error) => console.log(error))
