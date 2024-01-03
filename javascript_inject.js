a2d64b721ab92609a51afa717243083bead9cbb8
Keanu Clark <da1andonlyone13@gmail.com>
2024-01-03 02:14:40 -0500

    testing other userbase functionality: signIn and getDatabases

--- javascript_inject.js
+++ javascript_inject.js
@@ -29,6 +29,34 @@ async function start() {
 
 }
 
+async function return() { 
+
+  userbase.init({ 
+    appId: 'f3dece1f-eb4b-4a40-bab7-bf63746e1c8d'
+  })
+  
+  title.innerText = 'Retreiving account...'
+  await userbase.signIn({ 
+    username: "test_acct",
+    password: "test_psswrd"
+  })
+
+  title.innerText = 'Opening database...'
+  await userbase.openDatabase({
+    databaseName: 'demo',
+    changeHandler: function (items) {     
+      if (items.length) {
+        title.innerText = items[0].item
+      }
+    }
+  })
+
+  await userbase.getDatabases().then((databases) => {
+      console.log(databases)
+  })
+
+}
+
 // code for the start button
 window.addEventListener("DOMContentLoaded", (event) => {
     const button = document.getElementById('btn')
@@ -38,5 +66,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
       start()
     })
     const title = document.getElementById('title')
+
+    const button = document.getElementById('btn2')
+    button.addEventListener('click', function () { 
+      button.style.display = 'none'
+      if (sessionStorage) sessionStorage.clear()
+      return()
+    })
 });
 </script>
