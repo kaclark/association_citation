b1f39752f6d72af654ba0269f08840d20b24c17d
Keanu Clark <da1andonlyone13@gmail.com>
2024-02-09 20:11:05 -0500

    python todo.py --taskid 7456224352 --action getComments

--- todo.py
+++ todo.py
@@ -2,6 +2,18 @@ from todoist_api_python.api import TodoistAPI
 from todoist_api_python.models import Attachment, Comment, Task
 import numpy as np
 import sys
+import argparse
+
+parser = argparse.ArgumentParser(
+                    prog='todo',
+                    description='todoist cli',
+                    epilog='documentation: https://developer.todoist.com/rest/v2/?python#overview')
+
+parser.add_argument('-t', '--taskid')
+parser.add_argument('-a', '--action')  
+
+args = parser.parse_args()
+print(args.taskid, args.action)
 
 def comments_from_task_id(t_id):
     try:
@@ -11,7 +23,7 @@ def comments_from_task_id(t_id):
         print(error)
 
 #access api token
-with open("api.token", "r") as key_in:
-    api = TodoistAPI(str(key_in.readlines()[0].split("\n")[0]))
+#with open("api.token", "r") as key_in:
+#    api = TodoistAPI(str(key_in.readlines()[0].split("\n")[0]))
 
-print(comments_from_task_id(sys.argv[1]))
+#print(comments_from_task_id(sys.argv[1]))
