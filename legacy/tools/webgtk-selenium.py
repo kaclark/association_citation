10a53511fe1d65293c2ec3e89e6869d5f4631d49
Keanu Clark <da1andonlyone13@gmail.com>
2025-02-17 12:52:47 -0500

    headless webgtk seems not to exist?

--- legacy/tools/webgtk-selenium.py
+++ legacy/tools/webgtk-selenium.py
@@ -9,5 +9,8 @@ import sys
 from selenium import webdriver
 
 options = webdriver.WebKitGTKOptions()
+#options.add_argument('--headless=new')
 driver = webdriver.WebKitGTK(options=options)
 driver.get(sys.argv[1])
+print(driver.page_source)
+driver.close()
