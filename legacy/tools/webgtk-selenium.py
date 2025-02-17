a2168742ee2fb9adf72c750200df4f202aa696d8
kaclark <da1andonlyone13@gmail.com>
2025-02-17 14:21:54 -0500

    webkit tested on termux

--- legacy/tools/webgtk-selenium.py
+++ legacy/tools/webgtk-selenium.py
@@ -9,8 +9,9 @@ import sys
 from selenium import webdriver
 
 options = webdriver.WebKitGTKOptions()
-#options.add_argument('--headless=new')
+#options.add_argument('--headless')
 driver = webdriver.WebKitGTK(options=options)
-driver.get(sys.argv[1])
+#driver.get(sys.argv[1])
+driver.get("https://www.duckduckgo.com")
 print(driver.page_source)
 driver.close()
